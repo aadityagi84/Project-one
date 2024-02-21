8e673d83f7c1852da78bf93a5cf5c59b90513d4b
Aaditya Tyagi <tyagiadityagi@gmail.com>
2024-02-21 12:15:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-<script>   
-          new WOW().init();         
-          mobile:false;             
-</script>
+
+  $('#banner').owlCarousel({
+             loop:true,
+            //  left:true,
+             margin:30,
+             autoplay:true,
+             autoplayTimeout: 7000,
+             smartSpeed: 800,
+             nav:true, 
+            //  navText: ["<img src='img/a1.png'>","<img src='img/a2.png'>"],
+             dots:true,                
+             responsive:{
+                 0:{
+                     items:1
+                 },
+                 600:{
+                     items:1
+                 },
+                 1000:{
+                     items:1
+                 } 
+             }
+         });
